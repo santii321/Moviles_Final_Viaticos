188cb29c6ccafd60931e762846075ea4251343cc
santii321 <iefaz.santiagogaviria@gmail.com>
2022-12-15 10:41:39 -0500

    se agregan ultimas funciones, y se finaliza proyecto correctamente

---
+++ src/main/java/com/example/viaticosonline/controladores/ControladorEmpleado.java
@@ -0,0 +1,42 @@
+package com.example.viaticosonline.controladores;
+
+import com.example.viaticosonline.entidades.Empleado;
+import com.example.viaticosonline.servicios.ServiciosEmpleados;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/uribe/empelado")
+public class ConttroladorEmpleado {
+
+    @Autowired
+    ServiciosEmpleados serviciosEmpleados;
+
+    @PostMapping
+    public ResponseEntity<?> registar(@RequestBody Empleado empleado){
+        try{
+    return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(serviciosEmpleados.registrar(empleado));
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("{mensaje:Error al registrar empelado...}");
+        }
+    }
+    @GetMapping
+    public ResponseEntity<?> buscarEmpleado(){
+        try{
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(serviciosEmpleados.buscarTodos());
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("{mensaje:Error al registrar empelado...}");
+        }
+    }
+}
---
+++ src/main/java/com/example/viaticosonline/controladores/ControladorVieja.java
@@ -0,0 +1,2 @@
+package com.example.viaticosonline.controladores;public class ControladorVieja {
+}
